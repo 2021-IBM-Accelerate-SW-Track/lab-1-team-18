6b17163f568bb489ddfe43bf3ac1fa69c4c5d100
Destinee Ekanem <52009371+Desss64@users.noreply.github.com>
2021-06-16 11:24:20 -0400

    fixed file

--- src/App.test.js
+++ src/App.test.js
@@ -16,17 +16,17 @@ afterEach(() => {
   container = null;
 });
 
-test('test that App component renders', () => {
+test('that App component renders', () => {
   render(<App />, container);
  });
 
-test('test that new-item-button is a button', () => {
+test('that new-item-button is a button', () => {
   render(<App/>, container);
   const element = screen.getByTestId('new-item-button');
   expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
 });
 
-test('test that new-item-input is an input ', () => {
+test('that new-item-input is an input', () => {
   render(<App/>, container);
   const element = screen.getByTestId('new-item-input');
   expect(element.innerHTML.toLowerCase().includes("input")).toBe(true)
