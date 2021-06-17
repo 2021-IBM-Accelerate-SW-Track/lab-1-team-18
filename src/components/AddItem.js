4803f0f9d0f4fc755fe91558bf9601b23ebe9a1f
Treasure Ogundiran <ogundir2@msu.edu>
2021-06-16 21:47:14 -0400

    created a new branch with the backend functions. Renamed 'src/component/' to 'src/components/' added AddItem.js, Item.js, ItemList.js to 'src/components' modified index.html, App.css and App.js

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,20 +1,3 @@
-// import Header from './component/header';
-// import './App.css';
-// import { EnterListItem } from './component/enterListItem';
-// import React from "react";
-
-// function App(){
-//   return(
-//     <div className="container">
-//       <Header/>
-//       <EnterListItem/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import { useState } from "react"
 
 
