50ddccfb4e4e25191d6dd9e9fda7461f9e784ab7
Destinee Ekanem <52009371+Desss64@users.noreply.github.com>
2021-06-17 22:12:21 -0400

    put in time for added item

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -34,7 +34,7 @@ const AddItem = ({addItem, items}) => {
         if (!duplicate){
             // creates new date object and use the date object to create a time string
             const date = new Date();
-            const time = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}`;
+            const time = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
 
             // calls the addItem function and pass in the object {text,time}
             addItem({text, time});
